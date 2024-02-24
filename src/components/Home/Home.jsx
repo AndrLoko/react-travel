ea2b31bb6290dbeb4366e5b4ea2aa22e86b12b21
Andrew <andrewlokoffical@gmail.com>
2024-02-24 20:54:39 +0100

    Add total in Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './home.scss'
 import video from '../../assets/video.mp4'
+import { GrLocation } from "react-icons/gr";
 
 export default function Home() {
   return (
@@ -22,10 +23,30 @@ export default function Home() {
         </div>
 
         <div className="cardDiv grid">
-          <div className="destinationInput">
-            <label htmlFor="city">Search your destination</label>
-            <input type="input text" placeholder='Enter name here...' />
-          </div>
+
+            <div className="destinationInput">
+              <label htmlFor="city">Search your destination</label>
+              <input type="input text" placeholder='Enter name here...' />
+              <GrLocation className='icon'/>
+            </div>
+
+            <div className="dateInput">
+              <label htmlFor="date">Select your date</label>
+              <div className="input flex">
+                <input type="date" />
+              </div>
+            </div>
+
+            <div className="priceInput">
+                <div className="label_total flex">
+                  <label htmlFor="price">Max price:</label>
+                  <h3 className="total">$5000</h3>
+                </div>
+                <div className="input flex">
+                  <input type="range" max="5000" min="1000"/>
+                </div>
+            </div>
+
         </div>
       </div> 
     </section>
