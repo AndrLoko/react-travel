2bbd33d21f1195ef0b3167e2f7118872d3a3e2ea
Andrew <andrewlokoffical@gmail.com>
2024-02-26 18:00:25 +0100

    Add img and date-map

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,16 @@ import './main.scss'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <section className='main container section'>
+        <div className="secTitle">
+          <h3 className="title">
+            Most visited destionations
+          </h3>
+        </div>
+
+        <div className="secContent grid">
+          
+        </div>
+    </section>
   )
 }
