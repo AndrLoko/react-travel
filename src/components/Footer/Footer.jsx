548a791e5bb6b5989924ad2dcdcc424808e4fc19
Andrew <andrewlokoffical@gmail.com>
2024-03-04 20:55:30 +0100

    Add video in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './footer.scss'
 
+// import icons
+import { FiSend } from "react-icons/fi";
+
+// import images (video)
+import video2 from '../../assets/video2.mp4'
+
+
 export default function Footer() {
   return (
     <section className='footer'>
       <div className="videoDiv">
-        <video src=""></video>
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+            <div className="text">
+              <small>KEEP IN TOUCH</small>
+              <h2>Travel with us</h2>
+            </div>
+
+            <div className="inputDiv flex">
+              <input type="text" placeholder='Enter Email Address' />
+              <button className='btn flex' type='submit'>
+                SEND <FiSend className='icon'/>
+              </button>
+            </div>
+        </div>
       </div>
     </section>
   )
